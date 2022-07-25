eb7b6c6a71eb418d7e5deb3766da16323754735f
Bradford Knight <bradford.a.knight@gmail.com>
2022-07-25 17:51:02 -0400

    Adding Bootstrap grid to index page

--- controllers/places.js
+++ controllers/places.js
@@ -9,13 +9,13 @@ router.get('/', (req, res) => {
         city: 'Seattle',
         state: 'WA',
         cuisines: 'Thai, Pan-Asian',
-        pic: 'http://placekitten.com/250/250'
+        pic: '/images/Thai-Food.jpg'
       }, {
         name: 'Coding Cat Cafe',
         city: 'Phoenix',
         state: 'AZ',
         cuisines: 'Coffee, Bakery',
-        pic: 'http://placekitten.com/250/250'
+        pic: '/images/Coding-Cat.jpg'
       }]
       
     res.render('places/index', { places })
--- views/error404.jsx
+++ views/error404.jsx
@@ -7,6 +7,12 @@ function error404 () {
           <main>
               <h1>404: PAGE NOT FOUND</h1>
               <p>Oops, sorry, we can't find this page!</p>
+              <div>
+                <img src="/images/Dog-404.jpg" alt="Dog 404 Image"/>
+                <div>
+                  Photo by <a href="https://unsplash.com/@tcrock18">Tanner Crockett</a> on <a href="https://unsplash.com/">Unsplash</a>
+                </div>
+              </div>
           </main>
       </Def>
     )
